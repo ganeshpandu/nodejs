aa769d6272439c4d757894cff6d6a3f42a7049d9
ganni <ganni@gmail.com>
2025-04-13 11:22:12 +0000

    nodejs-31

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('goutami-poc-123 from ECS!');
+    res.end('goutami-poc from ECS!');
 });
 server.listen(3000, () => {
     console.log('Server running on port 3000');
