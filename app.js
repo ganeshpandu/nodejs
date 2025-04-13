df7b5899882dc0728b379a1c91cedea5581894db
ganni <ganni@gmail.com>
2025-04-13 07:09:47 +0000

    nodejs-23

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from ECS!');
+    res.end('Hiiiiiiiii from ECS!');
 });
 server.listen(3000, () => {
     console.log('Server running on port 3000');
